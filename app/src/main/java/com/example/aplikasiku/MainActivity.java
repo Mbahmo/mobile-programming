bb952daf1bdcd8ca7c12455db0f46f7c968fe41d
Mbahmo <anthonyleembahmo@gmail.com>
2020-03-28 23:14:43 +0800

    activity

--- app/src/main/java/com/example/aplikasiku/MainActivity.java
+++ app/src/main/java/com/example/aplikasiku/MainActivity.java
@@ -1,15 +1,61 @@
 package com.example.aplikasiku;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends Activity implements View.OnClickListener {
     private EditText EmailEditText, PasswordEditText;
+    private Button Login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EmailEditText    = (EditText)findViewById(R.id.txtusername);
+        PasswordEditText = (EditText)findViewById(R.id.txtpassword);
+        Login            = (Button)findViewById(R.id.btnprofil);
+        Login.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View arg0) {
+        CheckLogin(Login);
+    }
+
+    public void CheckLogin(View arg0){
+        final String email = EmailEditText.getText().toString();
+        if(!isValidEmail(email)){
+            EmailEditText.setError("Email Tidak Valid");
+        }
+        final String password = PasswordEditText.getText().toString();
+        if(!isValidPassword(password)){
+            PasswordEditText.setError("Password tidak boleh kosong atau harus lebih dari 4");
+        }
+        if(isValidEmail(email) && isValidPassword(password)){
+            Toast.makeText(getApplicationContext(), "Berhasil", Toast.LENGTH_LONG).show();
+            Intent ExplicitIntent=new Intent(MainActivity.this, Menu.class);
+            startActivity(ExplicitIntent);
+        }
+    }
+
+    public boolean isValidPassword(String password){
+        if(password != null && password.length()>=4){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isValidEmail(String email){
+        String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 }
