2bf8eb149494434586f58b63e9e7a7294c251a37
Mbahmo <anthonyleembahmo@gmail.com>
2020-03-28 21:50:09 +0800

    layout complete

--- app/src/main/java/com/example/aplikasiku/MainActivity.java
+++ app/src/main/java/com/example/aplikasiku/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.aplikasiku;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText EmailEditText, PasswordEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
