448c08bc52afeb5761a974d74789e27eb9bb4ee5
Mbahmo <anthonyleembahmo@gmail.com>
2020-05-02 03:14:11 +0800

    persegi fix

--- app/src/main/java/com/example/aplikasiku/Persegi.java
+++ app/src/main/java/com/example/aplikasiku/Persegi.java
@@ -5,10 +5,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class Persegi extends Activity implements View.OnClickListener {
     private EditText Sisi;
+    private TextView Hasil;
     private Integer Input;
     private Button HitungLuas, HitungKeliling;
     private String Username, Password;
@@ -17,9 +18,13 @@ public class Persegi extends Activity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_persegi);
-        Sisi = findViewById(R.id.txtsisi);
+
+        Sisi = findViewById(R.id.txtsisipersegi);
+        Hasil = findViewById(R.id.txthasilpersegi);
+
         HitungLuas = findViewById(R.id.btnhitungluaspersegi);
         HitungLuas.setOnClickListener(this);
+
         HitungKeliling = findViewById(R.id.btnhitungkelilingpersegi);
         HitungKeliling.setOnClickListener(this);
     }
@@ -30,11 +35,11 @@ public class Persegi extends Activity implements View.OnClickListener {
 
         switch (arg0.getId()) {
             case R.id.btnhitungluaspersegi:
-                Toast.makeText(getApplicationContext(), "Hasil Luas Persegi: " + Input * Input, Toast.LENGTH_LONG).show();
+                Hasil.setText("Hasil Luas Persegi: " + 4 * Input);
                 break;
 
             case R.id.btnhitungkelilingpersegi:
-                Toast.makeText(getApplicationContext(), "Hasil Keliling Persegi: " + 4 * Input, Toast.LENGTH_LONG).show();
+                Hasil.setText("Hasil Keliling Persegi: " + 4 * Input);
                 break;
         }
     }
